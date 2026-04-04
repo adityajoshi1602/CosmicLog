e3271d9344025ab4e2b6416cf3b672f650a57fb1
Aditya joshi <adityajoshi1602@gmail.com>
2026-04-05 02:51:03 +0530

    added auth routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const cors = require('cors');
+const authroute= require('./routes/auth.routes')
 const app = express()
+
+app.use(cors());
 app.use(express.json())
+
+app.use('/api/auth',)
 
 module.exports = app
